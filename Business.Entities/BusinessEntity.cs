11f5a5aa536576b0887de95d0def194550d4308b
JuaniPucheta <nachop2000@gmail.com>
2021-05-07 16:09:20 -0300

    TP2Lab02 - etapa 1|

---
+++ Business.Entities/BusinessEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class BusinessEntity
+    {
+        public BusinessEntity()
+        {
+            this.State = States.New;
+        }
+
+        private int _ID;
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+
+        private States _State;
+        public States State;
+
+        public enum States
+        {
+            Deleted,
+            New,
+            Modified,
+            Unmodified
+        }
+    }
+}
---
+++ Business.Entities/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class Usuario
+    {
+        private string nombreUsuario, clave, nombre, apellido, email;
+        private bool habilitado;
+
+        public string NombreUsuario { get => nombreUsuario; set => nombreUsuario = value; }
+        public string Clave { get => clave; set => clave = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Email { get => email; set => email = value; }
+        public bool Habilitado { get => habilitado; set => habilitado = value; }
+
+
+    }
+}
