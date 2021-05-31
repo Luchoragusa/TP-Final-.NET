e153aca272cb8f8d12aa288000b5e0bedc5edf08
JuaniPucheta <nachop2000@gmail.com>
2021-05-31 14:24:25 -0300

    TP2 - Lab04 - error en Usuario.cs (UI.Desktop) sobre el GetAll

--- Business.Logic/UsuarioLogic.cs
+++ Business.Logic/UsuarioLogic.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Business.Logic
 {
     public class UsuarioLogic : BusinessLogic
---
+++ UI.Desktop/Usuarios.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace UI.Desktop
+{
+    partial class Usuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tcUsuarios = new System.Windows.Forms.ToolStripContainer();
+            this.tlUsuarios = new System.Windows.Forms.TableLayoutPanel();
+            this.dgvUsuarios = new System.Windows.Forms.DataGridView();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.tsUsuarios = new System.Windows.Forms.ToolStrip();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.usuario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.habilitado = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.tcUsuarios.ContentPanel.SuspendLayout();
+            this.tcUsuarios.TopToolStripPanel.SuspendLayout();
+            this.tcUsuarios.SuspendLayout();
+            this.tlUsuarios.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tcUsuarios
+            // 
+            // 
+            // tcUsuarios.ContentPanel
+            // 
+            this.tcUsuarios.ContentPanel.Controls.Add(this.tlUsuarios);
+            this.tcUsuarios.ContentPanel.Size = new System.Drawing.Size(848, 487);
+            this.tcUsuarios.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tcUsuarios.Location = new System.Drawing.Point(0, 0);
+            this.tcUsuarios.Name = "tcUsuarios";
+            this.tcUsuarios.Size = new System.Drawing.Size(848, 512);
+            this.tcUsuarios.TabIndex = 0;
+            this.tcUsuarios.Text = "toolStripContainer1";
+            // 
+            // tcUsuarios.TopToolStripPanel
+            // 
+            this.tcUsuarios.TopToolStripPanel.Controls.Add(this.tsUsuarios);
+            // 
+            // tlUsuarios
+            // 
+            this.tlUsuarios.ColumnCount = 2;
+            this.tlUsuarios.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlUsuarios.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tlUsuarios.Controls.Add(this.dgvUsuarios, 0, 0);
+            this.tlUsuarios.Controls.Add(this.btnActualizar, 0, 1);
+            this.tlUsuarios.Controls.Add(this.btnSalir, 1, 1);
+            this.tlUsuarios.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlUsuarios.Location = new System.Drawing.Point(0, 0);
+            this.tlUsuarios.Name = "tlUsuarios";
+            this.tlUsuarios.RowCount = 2;
+            this.tlUsuarios.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlUsuarios.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tlUsuarios.Size = new System.Drawing.Size(848, 487);
+            this.tlUsuarios.TabIndex = 0;
+            // 
+            // dgvUsuarios
+            // 
+            this.dgvUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsuarios.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.nombre,
+            this.apellido,
+            this.usuario,
+            this.email,
+            this.habilitado});
+            this.tlUsuarios.SetColumnSpan(this.dgvUsuarios, 2);
+            this.dgvUsuarios.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvUsuarios.Location = new System.Drawing.Point(3, 3);
+            this.dgvUsuarios.Name = "dgvUsuarios";
+            this.dgvUsuarios.Size = new System.Drawing.Size(842, 452);
+            this.dgvUsuarios.TabIndex = 0;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnActualizar.Location = new System.Drawing.Point(689, 461);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 1;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(770, 461);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // tsUsuarios
+            // 
+            this.tsUsuarios.Dock = System.Windows.Forms.DockStyle.None;
+            this.tsUsuarios.Location = new System.Drawing.Point(3, 0);
+            this.tsUsuarios.Name = "tsUsuarios";
+            this.tsUsuarios.Size = new System.Drawing.Size(111, 25);
+            this.tsUsuarios.TabIndex = 0;
+            // 
+            // id
+            // 
+            this.id.DataPropertyName = "ID";
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
+            // nombre
+            // 
+            this.nombre.DataPropertyName = "Nombre";
+            this.nombre.HeaderText = "Nombre";
+            this.nombre.Name = "nombre";
+            this.nombre.ReadOnly = true;
+            // 
+            // apellido
+            // 
+            this.apellido.DataPropertyName = "Apellido";
+            this.apellido.HeaderText = "Apellido";
+            this.apellido.Name = "apellido";
+            this.apellido.ReadOnly = true;
+            // 
+            // usuario
+            // 
+            this.usuario.DataPropertyName = "NombreUsuario";
+            this.usuario.HeaderText = "Usuario";
+            this.usuario.Name = "usuario";
+            this.usuario.ReadOnly = true;
+            // 
+            // email
+            // 
+            this.email.DataPropertyName = "EMail";
+            this.email.HeaderText = "EMail";
+            this.email.Name = "email";
+            this.email.ReadOnly = true;
+            // 
+            // habilitado
+            // 
+            this.habilitado.DataPropertyName = "Habilitado";
+            this.habilitado.HeaderText = "Habilitado";
+            this.habilitado.Name = "habilitado";
+            this.habilitado.ReadOnly = true;
+            // 
+            // Usuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(848, 512);
+            this.Controls.Add(this.tcUsuarios);
+            this.Name = "Usuarios";
+            this.Text = "Usuarios";
+            this.Load += new System.EventHandler(this.Usuarios_Load);
+            this.tcUsuarios.ContentPanel.ResumeLayout(false);
+            this.tcUsuarios.TopToolStripPanel.ResumeLayout(false);
+            this.tcUsuarios.TopToolStripPanel.PerformLayout();
+            this.tcUsuarios.ResumeLayout(false);
+            this.tcUsuarios.PerformLayout();
+            this.tlUsuarios.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer tcUsuarios;
+        private System.Windows.Forms.ToolStrip tsUsuarios;
+        private System.Windows.Forms.TableLayoutPanel tlUsuarios;
+        private System.Windows.Forms.DataGridView dgvUsuarios;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn usuario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn habilitado;
+    }
+}
