4557d3bfe02bf68af9e783cf8d9590cae2262c81
JuaniPucheta <nachop2000@gmail.com>
2021-06-28 17:52:16 -0300

    UsuarioDesktop andando 17:52hs

---
+++ UI.Desktop/UsuarioDesktop.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UI.Desktop
+{
+    public partial class UsuarioDesktop : UI.Desktop.ApplicationForm
+    {
+        public UsuarioDesktop()
+        {
+            InitializeComponent();
+        }
+
+        //enum
+        public enum ModoForm
+        {
+            Alta,
+            Baja,
+            Modificacion,
+            Consulta
+        };
+
+        //propiedad
+        public ModoForm _modoform
+        {
+            get
+            {
+                return _modoform;
+            }
+            set
+            {
+                _modoform = value;
+            }
+        }
+
+        public virtual void MapearDeDatos() { }
+        public virtual void MapearADatos() { }
+        public virtual void GuardarCambios() { }
+        public virtual bool Validar() { return false; }
+        public void Notificar(string titulo, string mensaje, MessageBoxButtons
+        botones, MessageBoxIcon icono)
+        {
+            MessageBox.Show(mensaje, titulo, botones, icono);
+        }
+        public void Notificar(string mensaje, MessageBoxButtons botones,
+        MessageBoxIcon icono)
+        {
+            this.Notificar(this.Text, mensaje, botones, icono);
+        }
+
+    }
+}
