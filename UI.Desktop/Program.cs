e0d57f19c81461e3af73fb5188e8800a7e609848
JuaniPucheta <nachop2000@gmail.com>
2021-07-26 16:16:36 -0300

    26/7

--- UI.Desktop/Program.cs
+++ UI.Desktop/Program.cs
@@ -17,9 +17,9 @@ namespace UI.Desktop
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new frmLogin());
-            //Application.Run(new formMain());
+            Application.Run(new formMain());
             //Application.Run(new Usuarios());  
-            Application.Run(new Comisiones());
+            //Application.Run(new Comisiones());
             //Application.Run(new Materia());
         }
     }
