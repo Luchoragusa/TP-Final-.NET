bd56cbef30840f90afc6bd0c1bf0245471a7b6d3
Luciano Ragusa <80652919+Luchoragusa@users.noreply.github.com>
2021-09-23 00:28:41 -0300

    format

--- UI.Web/Usuarios.aspx.designer.cs
+++ UI.Web/Usuarios.aspx.designer.cs
@@ -40,5 +40,14 @@ namespace UI.Web
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gridView;
+
+        /// <summary>
+        /// Control SqlDataSource1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
