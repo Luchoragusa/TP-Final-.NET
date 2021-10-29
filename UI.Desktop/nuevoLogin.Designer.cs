70228505b2ff8b1ab8c0c0d744ac5f49b810ecef
JuaniPucheta <nachop2000@gmail.com>
2021-10-29 16:41:47 -0300

    nuevoLogin hecho y funcionando

--- UI.Desktop/nuevoLogin.Designer.cs
+++ UI.Desktop/nuevoLogin.Designer.cs
@@ -30,13 +30,13 @@ namespace UI.Desktop
         private void InitializeComponent()
         {
             this.panelExt = new System.Windows.Forms.Panel();
-            this.label2 = new System.Windows.Forms.Label();
-            this.lnkOlvidaPass = new System.Windows.Forms.LinkLabel();
-            this.lblNombreUsuario = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtUsuario = new System.Windows.Forms.TextBox();
-            this.txtPass = new System.Windows.Forms.TextBox();
             this.btnIngresar = new System.Windows.Forms.Button();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblNombreUsuario = new System.Windows.Forms.Label();
+            this.lnkOlvidaPass = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
             this.panelExt.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -56,66 +56,6 @@ namespace UI.Desktop
             this.panelExt.Size = new System.Drawing.Size(428, 297);
             this.panelExt.TabIndex = 0;
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(66, 37);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(312, 36);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "¡Bienvenido al Sistema! \r\nPor favor digite su información de Ingreso";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // lnkOlvidaPass
-            // 
-            this.lnkOlvidaPass.AutoSize = true;
-            this.lnkOlvidaPass.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lnkOlvidaPass.Location = new System.Drawing.Point(66, 237);
-            this.lnkOlvidaPass.Name = "lnkOlvidaPass";
-            this.lnkOlvidaPass.Size = new System.Drawing.Size(138, 16);
-            this.lnkOlvidaPass.TabIndex = 7;
-            this.lnkOlvidaPass.TabStop = true;
-            this.lnkOlvidaPass.Text = "Olvido su contraseña?";
-            // 
-            // lblNombreUsuario
-            // 
-            this.lblNombreUsuario.AutoSize = true;
-            this.lblNombreUsuario.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombreUsuario.Location = new System.Drawing.Point(94, 92);
-            this.lblNombreUsuario.Name = "lblNombreUsuario";
-            this.lblNombreUsuario.Size = new System.Drawing.Size(120, 16);
-            this.lblNombreUsuario.TabIndex = 8;
-            this.lblNombreUsuario.Text = "Nombre de usuario";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(94, 163);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(75, 16);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Contraseña";
-            // 
-            // txtUsuario
-            // 
-            this.txtUsuario.BackColor = System.Drawing.SystemColors.InactiveBorder;
-            this.txtUsuario.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsuario.Location = new System.Drawing.Point(97, 124);
-            this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(247, 22);
-            this.txtUsuario.TabIndex = 10;
-            // 
-            // txtPass
-            // 
-            this.txtPass.BackColor = System.Drawing.SystemColors.InactiveBorder;
-            this.txtPass.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPass.Location = new System.Drawing.Point(97, 190);
-            this.txtPass.Name = "txtPass";
-            this.txtPass.Size = new System.Drawing.Size(247, 22);
-            this.txtPass.TabIndex = 11;
-            // 
             // btnIngresar
             // 
             this.btnIngresar.BackColor = System.Drawing.Color.CornflowerBlue;
@@ -128,6 +68,69 @@ namespace UI.Desktop
             this.btnIngresar.TabIndex = 12;
             this.btnIngresar.Text = "Ingresar";
             this.btnIngresar.UseVisualStyleBackColor = false;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // txtPass
+            // 
+            this.txtPass.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtPass.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(97, 190);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(247, 22);
+            this.txtPass.TabIndex = 11;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtUsuario.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(97, 124);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(247, 22);
+            this.txtUsuario.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(94, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Contraseña";
+            // 
+            // lblNombreUsuario
+            // 
+            this.lblNombreUsuario.AutoSize = true;
+            this.lblNombreUsuario.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreUsuario.Location = new System.Drawing.Point(94, 92);
+            this.lblNombreUsuario.Name = "lblNombreUsuario";
+            this.lblNombreUsuario.Size = new System.Drawing.Size(120, 16);
+            this.lblNombreUsuario.TabIndex = 8;
+            this.lblNombreUsuario.Text = "Nombre de usuario";
+            // 
+            // lnkOlvidaPass
+            // 
+            this.lnkOlvidaPass.AutoSize = true;
+            this.lnkOlvidaPass.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkOlvidaPass.Location = new System.Drawing.Point(66, 237);
+            this.lnkOlvidaPass.Name = "lnkOlvidaPass";
+            this.lnkOlvidaPass.Size = new System.Drawing.Size(138, 16);
+            this.lnkOlvidaPass.TabIndex = 7;
+            this.lnkOlvidaPass.TabStop = true;
+            this.lnkOlvidaPass.Text = "Olvido su contraseña?";
+            this.lnkOlvidaPass.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkOlvidaPass_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(66, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(312, 36);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "¡Bienvenido al Sistema! \r\nPor favor digite su información de Ingreso";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // nuevoLogin
             // 
