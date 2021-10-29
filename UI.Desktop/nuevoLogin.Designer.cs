bd1be91d42f552130cbc375eb8fdc8a53d87bf5e
Maite <mailicatta@gmail.com>
2021-10-29 16:12:40 -0300

    nuevo Login ok

---
+++ UI.Desktop/nuevoLogin.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace UI.Desktop
+{
+    partial class nuevoLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelExt = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lnkOlvidaPass = new System.Windows.Forms.LinkLabel();
+            this.lblNombreUsuario = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.panelExt.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelExt
+            // 
+            this.panelExt.BackColor = System.Drawing.Color.AliceBlue;
+            this.panelExt.Controls.Add(this.btnIngresar);
+            this.panelExt.Controls.Add(this.txtPass);
+            this.panelExt.Controls.Add(this.txtUsuario);
+            this.panelExt.Controls.Add(this.label3);
+            this.panelExt.Controls.Add(this.lblNombreUsuario);
+            this.panelExt.Controls.Add(this.lnkOlvidaPass);
+            this.panelExt.Controls.Add(this.label2);
+            this.panelExt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelExt.Location = new System.Drawing.Point(0, 0);
+            this.panelExt.Name = "panelExt";
+            this.panelExt.Size = new System.Drawing.Size(428, 297);
+            this.panelExt.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(66, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(312, 36);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "¡Bienvenido al Sistema! \r\nPor favor digite su información de Ingreso";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lnkOlvidaPass
+            // 
+            this.lnkOlvidaPass.AutoSize = true;
+            this.lnkOlvidaPass.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkOlvidaPass.Location = new System.Drawing.Point(66, 237);
+            this.lnkOlvidaPass.Name = "lnkOlvidaPass";
+            this.lnkOlvidaPass.Size = new System.Drawing.Size(138, 16);
+            this.lnkOlvidaPass.TabIndex = 7;
+            this.lnkOlvidaPass.TabStop = true;
+            this.lnkOlvidaPass.Text = "Olvido su contraseña?";
+            // 
+            // lblNombreUsuario
+            // 
+            this.lblNombreUsuario.AutoSize = true;
+            this.lblNombreUsuario.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreUsuario.Location = new System.Drawing.Point(94, 92);
+            this.lblNombreUsuario.Name = "lblNombreUsuario";
+            this.lblNombreUsuario.Size = new System.Drawing.Size(120, 16);
+            this.lblNombreUsuario.TabIndex = 8;
+            this.lblNombreUsuario.Text = "Nombre de usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(94, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtUsuario.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(97, 124);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(247, 22);
+            this.txtUsuario.TabIndex = 10;
+            // 
+            // txtPass
+            // 
+            this.txtPass.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtPass.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(97, 190);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(247, 22);
+            this.txtPass.TabIndex = 11;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnIngresar.FlatAppearance.BorderSize = 0;
+            this.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIngresar.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.Location = new System.Drawing.Point(275, 230);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(103, 23);
+            this.btnIngresar.TabIndex = 12;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = false;
+            // 
+            // nuevoLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 297);
+            this.Controls.Add(this.panelExt);
+            this.Name = "nuevoLogin";
+            this.Text = "nuevoLogin";
+            this.panelExt.ResumeLayout(false);
+            this.panelExt.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelExt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblNombreUsuario;
+        private System.Windows.Forms.LinkLabel lnkOlvidaPass;
+    }
+}
---
+++ UI.Desktop/nuevoLogin.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Entities;
+using Business.Logic;
+
+namespace UI.Desktop
+{
+    public partial class nuevoLogin : Form
+    {
+        public nuevoLogin()
+        {
+            InitializeComponent();
+            US = new UsuarioLogic();
+        }
+
+       
+        public UsuarioLogic US
+        {
+            get; set;
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            Usuario usuario = new Usuario();
+            int tipo = 0;
+
+            nuevoMaincs nm = null;
+
+            usuario.NombreUsuario = this.txtUsuario.Text;
+            usuario.Clave = this.txtPass.Text;
+
+            UsuarioLogic loginUsuario = new UsuarioLogic();
+
+            tipo = loginUsuario.login(usuario);
+
+            if (tipo != -1)
+            {
+                if (tipo == 1)      //alumno
+                {
+                    MessageBox.Show("Bienvenido al sistema, ALUMNO.", "Login", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.DialogResult = DialogResult.OK;
+
+                }
+                else if (tipo == 2) //docente
+                {
+                    MessageBox.Show("Bienvenido al sistema, DOCENTE.", "Login", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.DialogResult = DialogResult.OK;
+                }
+                else if (tipo == 3) //admin
+                {
+                    MessageBox.Show("Bienvenido al sistema, ADMINISTRADOR.", "Login", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.DialogResult = DialogResult.OK;
+                }
+                else if (tipo == 4) //admin
+                {
+                    MessageBox.Show("Bienvenido al sistema, PERSONA.", "Login", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.DialogResult = DialogResult.OK;
+                }
+
+                nm = new nuevoMaincs(tipo, usuario);
+                nm.ShowDialog();
+                //this.Dispose();
+            }
+            else
+            {
+                MessageBox.Show("Usuario y/o contraseña incorrectos", "Login", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void lnkOlvidaPass_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Es Ud. un usuario muy descuidado, haga memoria", "Olvidé mi contraseña",
+            MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+        }
+    }
+}
