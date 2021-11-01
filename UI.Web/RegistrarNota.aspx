e91d0e1b95dd8de6d45e1cb236f04b5ee61c09af
JuaniPucheta <nachop2000@gmail.com>
2021-11-01 18:31:22 -0300

    RegistarNota.aspx funcionando god

--- UI.Web/RegistrarNota.aspx
+++ UI.Web/RegistrarNota.aspx
@@ -13,7 +13,7 @@
                <asp:GridView ID="gridViewCursosDelDocente" runat="server" AutoGenerateColumns="False"
                  SelectedRowStyle-BackColor="Black"
                  SelectedRowStyle-ForeColor="White"
-                 DataKeysNames="ID" DataKeyNames="ID" OnSelectedIndexChanged="gridView_SelectedIndexChanged" BackColor="White" BorderColor="#336666" BorderStyle="Double" BorderWidth="3px" CellPadding="4" GridLines="Horizontal">
+                 DataKeysNames="ID" DataKeyNames="ID" OnSelectedIndexChanged="gridViewCursos_SelectedIndexChanged" BackColor="White" BorderColor="#336666" BorderStyle="Double" BorderWidth="3px" CellPadding="4" GridLines="Horizontal">
                <Columns>
                    <asp:BoundField HeaderText="ID del Curso" DataField="ID" />
                         <asp:BoundField HeaderText="AnioCalendario" DataField="AnioCalendario" />
@@ -34,15 +34,18 @@
 
                 </asp:GridView>
 
+
+
         <asp:Panel ID="Panel2" runat="server">
             <asp:Label ID="lblAlumnosAsignados" runat="server" Text="Alumnos asignados al curso:"></asp:Label>
                  <asp:GridView ID="gridViewAlumnosDelCursoDelDocente" runat="server" AutoGenerateColumns="False"
                  SelectedRowStyle-BackColor="Black"
                  SelectedRowStyle-ForeColor="White"
-                 DataKeysNames="ID" DataKeyNames="ID" OnSelectedIndexChanged="gridView_SelectedIndexChanged" BackColor="White" BorderColor="#336666" BorderStyle="Double" BorderWidth="3px" CellPadding="4" GridLines="Horizontal">
+                 DataKeysNames="ID" DataKeyNames="ID" OnSelectedIndexChanged="gridViewAlumnos_SelectedIndexChanged" BackColor="White" BorderColor="#336666" BorderStyle="Double" BorderWidth="3px" CellPadding="4" GridLines="Horizontal">
                <Columns>
                         <asp:BoundField HeaderText="Nombre" DataField="NombrePersona" />
                         <asp:BoundField HeaderText="Apellido" DataField="ApellidoPersona" />
+                        <asp:BoundField HeaderText="Legajo" DataField="legajoPersona" />
                         <asp:BoundField HeaderText="Nota" DataField="Nota" />
                         <asp:BoundField HeaderText="Condicion" DataField="Condicion" />
                         <asp:CommandField SelectText="Seleccionar" ShowSelectButton="True" />
@@ -92,12 +95,12 @@
             &nbsp;
             <asp:Button ID="cancelarButton" runat="server" class="btn btn-outline-primary" Text="Cancelar" OnClick="cancelarButton_Click" />
 
-         <asp:Panel ID="Panel4" runat="server" HorizontalAlign="Center">
-        <asp:Button ID="btnVolver" runat="server" BackColor="#006666" Font-Bold="True" ForeColor="White" Text="Volver al menu" Width="121px" OnClick="btnVolver_Click" />
-             </asp:Panel>
         </asp:Panel>
 
         </asp:Panel>
-    </asp:Panel>              
+    </asp:Panel>    
+            <asp:Panel ID="Panel4" runat="server" HorizontalAlign="Center" Visible ="true">
+        <asp:Button ID="btnVolver" runat="server" BackColor="#006666" Font-Bold="True" ForeColor="White" Text="Volver al menu" Width="121px" OnClick="btnVolver_Click" />
+             </asp:Panel>
 
 </asp:Content>
--- UI.Web/RegistrarNota.aspx.cs
+++ UI.Web/RegistrarNota.aspx.cs
@@ -15,28 +15,28 @@ namespace UI.Web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            btnVolver.Visible = true;
             if (!Page.IsPostBack)
             {
+                btnVolver.Visible = true;
                 LoadGridCursos();
                 if (this.gridViewCursosDelDocente.SelectedIndex == -1)
                 {
                     this.Panel2.Visible = false;
                 }
             }
+            else
+            {
+                btnVolver.Visible = true;
+            }
         }
         private void LoadGridCursos()
         {
+            btnVolver.Visible = true;
             Usuario docente = (Usuario)Session["usuario"];
             Business.Logic.EntidadesLogic.CursoLogic cl = new Business.Logic.EntidadesLogic.CursoLogic();
             this.gridViewCursosDelDocente.DataSource = cl.GetAllDocente(docente);
             this.gridViewCursosDelDocente.DataBind();
-        }
-
-        protected void gridView_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            this.SelectedID = (int)this.gridViewCursosDelDocente.SelectedValue;
-            LoadGridAlumnos();
-            this.Panel2.Visible = true;
         }
 
         private void LoadGridAlumnos()
@@ -49,5 +49,79 @@ namespace UI.Web
             this.gridViewAlumnosDelCursoDelDocente.DataSource = al.GetAllByCurso(curso);
             this.gridViewAlumnosDelCursoDelDocente.DataBind();
         }
+
+        public Alumnos_Inscripciones Entity
+        {
+            get;set;
+        }
+
+        private int SelectedIDAlumno
+        {
+            get
+            {
+                if (this.ViewState["SelectedIDAlumno"] != null)
+                    return (int)this.ViewState["SelectedIDAlumno"];
+                else
+                    return 0;
+            }
+            set
+            {
+                this.ViewState["SelectedIDAlumno"] = value;
+            }
+        }
+
+        protected void acceptaButton_Click(object sender, EventArgs e)
+        {
+            Alumno_InscripcionLogic alumnoLogic = new Alumno_InscripcionLogic();
+            this.Entity = alumnoLogic.GetOne(SelectedIDAlumno);
+            this.Entity.State = BusinessEntity.States.Modified;
+            this.LoadEntity(this.Entity);
+            this.SaveEntity(this.Entity);
+            this.LoadGridAlumnos();
+        }
+
+        protected void cancelarButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("RegistrarNota.aspx");
+        }
+
+        private void SaveEntity(Alumnos_Inscripciones alumno)
+        {
+            try
+            {
+                Alumno_InscripcionLogic alumnoLogic = new Alumno_InscripcionLogic();
+                alumnoLogic.Save(alumno);
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>window.alert('" + ex.Message + "');</script>");
+            }
+        }
+    
+        private void LoadEntity(Alumnos_Inscripciones alumno)
+        {
+            alumno.Nota = this.DropDownListNotas.SelectedValue;
+            alumno.Condicion = this.DropDownListCondicion.SelectedValue;
+        }
+
+        protected void gridViewCursos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.SelectedID = (int)this.gridViewCursosDelDocente.SelectedValue;
+            LoadGridAlumnos();
+            this.Panel2.Visible = true;
+        }
+
+        protected void gridViewAlumnos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.SelectedIDAlumno = (int)this.gridViewAlumnosDelCursoDelDocente.SelectedValue;
+        }
+
+        protected void btnVolver_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("MenuPrincipal.aspx");
+        }
+
+
     }
 }
