7d5afb9a921ca842239397acc3d9335cded85617
Luciano Ragusa <80652919+Luchoragusa@users.noreply.github.com>
2021-11-05 19:03:08 -0300

    Materia.cs

--- UI.Desktop/Materia/Materia.cs
+++ UI.Desktop/Materia/Materia.cs
@@ -33,22 +33,18 @@ namespace UI.Desktop
                 MessageBox.Show(ex.Message + "Error detected: ", "Ha habido un error interno.", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }            
         }
-
         private void btnActualizar_Click(object sender, EventArgs e)
         {
             this.Listar();
         }
-
         private void btnSalir_Click(object sender, EventArgs e)
         {
             this.Close();
         }
-
         private void Materia_Load(object sender, EventArgs e)
         {
             Listar();
         }
-
         private void tsbNuevo_Click_1(object sender, EventArgs e)
         {
             MateriaDesktop formMDesktop = new MateriaDesktop(ApplicationForm.ModoForm.Alta);
@@ -62,7 +58,6 @@ namespace UI.Desktop
                 throw ex;
             }            
         }
-
         private void tsbEditar_Click_1(object sender, EventArgs e)
         {
             if (this.dgvMaterias.SelectedRows.Count == 0)
@@ -72,7 +67,6 @@ namespace UI.Desktop
 
             }
             int ID = ((Business.Entities.Materia)this.dgvMaterias.SelectedRows[0].DataBoundItem).ID;
-
             MateriaDesktop formMateria = new MateriaDesktop(ID, ApplicationForm.ModoForm.Modificacion);
             try
             {
@@ -84,7 +78,6 @@ namespace UI.Desktop
                 MessageBox.Show(ex.Message + "Error al editar ", "Ha habido un error interno.", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-
         private void tsbEliminar_Click_1(object sender, EventArgs e)
         {
             if (this.dgvMaterias.SelectedRows.Count == 0)
@@ -93,7 +86,6 @@ namespace UI.Desktop
                 return;
             }
             int ID = ((Business.Entities.Materia)this.dgvMaterias.SelectedRows[0].DataBoundItem).ID;
-
             MateriaDesktop formMateria = new MateriaDesktop(ID, ApplicationForm.ModoForm.Baja);
             try
             {
