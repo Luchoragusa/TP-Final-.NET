42f2776cf2ab7dbcb3f89e93ce2f220ded06b098
Luciano Ragusa <80652919+Luchoragusa@users.noreply.github.com>
2021-11-06 19:20:07 -0300

    name form Docente Cruso

--- UI.Desktop/RegistroNotas/RegistrarNotasMat.Designer.cs
+++ UI.Desktop/RegistroNotas/RegistrarNotasMat.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace UI.Desktop.RegistroNotas
 {
-    partial class RegistroNotas
+    partial class RegistrarNotasMat
     {
         /// <summary>
         /// Required designer variable.
@@ -31,19 +31,26 @@ namespace UI.Desktop.RegistroNotas
         {
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegistroNotas));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegistrarNotasMat));
             this.tcPlanes = new System.Windows.Forms.ToolStripContainer();
             this.tlPlanes = new System.Windows.Forms.TableLayoutPanel();
-            this.dgvRN = new System.Windows.Forms.DataGridView();
+            this.dgvDocenteCurso = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IDCurso = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IdDocente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cargo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.IDPlan = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btnSalir = new System.Windows.Forms.Button();
             this.btnActualizar = new System.Windows.Forms.Button();
             this.tsPlanes = new System.Windows.Forms.ToolStrip();
+            this.tsbNuevo = new System.Windows.Forms.ToolStripButton();
             this.tsbEditar = new System.Windows.Forms.ToolStripButton();
+            this.tsbEliminar = new System.Windows.Forms.ToolStripButton();
             this.tcPlanes.ContentPanel.SuspendLayout();
             this.tcPlanes.TopToolStripPanel.SuspendLayout();
             this.tcPlanes.SuspendLayout();
             this.tlPlanes.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvRN)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocenteCurso)).BeginInit();
             this.tsPlanes.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -58,7 +65,7 @@ namespace UI.Desktop.RegistroNotas
             this.tcPlanes.Location = new System.Drawing.Point(0, 0);
             this.tcPlanes.Name = "tcPlanes";
             this.tcPlanes.Size = new System.Drawing.Size(800, 450);
-            this.tcPlanes.TabIndex = 1;
+            this.tcPlanes.TabIndex = 2;
             this.tcPlanes.Text = "toolStripContainer1";
             // 
             // tcPlanes.TopToolStripPanel
@@ -70,7 +77,7 @@ namespace UI.Desktop.RegistroNotas
             this.tlPlanes.ColumnCount = 2;
             this.tlPlanes.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tlPlanes.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tlPlanes.Controls.Add(this.dgvRN, 0, 0);
+            this.tlPlanes.Controls.Add(this.dgvDocenteCurso, 0, 0);
             this.tlPlanes.Controls.Add(this.btnSalir, 1, 1);
             this.tlPlanes.Controls.Add(this.btnActualizar, 0, 1);
             this.tlPlanes.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -82,9 +89,9 @@ namespace UI.Desktop.RegistroNotas
             this.tlPlanes.Size = new System.Drawing.Size(800, 425);
             this.tlPlanes.TabIndex = 0;
             // 
-            // dgvRN
+            // dgvDocenteCurso
             // 
-            this.dgvRN.BackgroundColor = System.Drawing.Color.LightSteelBlue;
+            this.dgvDocenteCurso.BackgroundColor = System.Drawing.Color.LightSteelBlue;
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
             dataGridViewCellStyle1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -92,9 +99,15 @@ namespace UI.Desktop.RegistroNotas
             dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvRN.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
-            this.dgvRN.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.tlPlanes.SetColumnSpan(this.dgvRN, 2);
+            this.dgvDocenteCurso.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvDocenteCurso.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDocenteCurso.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.IDCurso,
+            this.IdDocente,
+            this.Cargo,
+            this.IDPlan});
+            this.tlPlanes.SetColumnSpan(this.dgvDocenteCurso, 2);
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
             dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -102,13 +115,46 @@ namespace UI.Desktop.RegistroNotas
             dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvRN.DefaultCellStyle = dataGridViewCellStyle2;
-            this.dgvRN.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvRN.Location = new System.Drawing.Point(3, 3);
-            this.dgvRN.MultiSelect = false;
-            this.dgvRN.Name = "dgvRN";
-            this.dgvRN.Size = new System.Drawing.Size(794, 390);
-            this.dgvRN.TabIndex = 0;
+            this.dgvDocenteCurso.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvDocenteCurso.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvDocenteCurso.Location = new System.Drawing.Point(3, 3);
+            this.dgvDocenteCurso.MultiSelect = false;
+            this.dgvDocenteCurso.Name = "dgvDocenteCurso";
+            this.dgvDocenteCurso.Size = new System.Drawing.Size(794, 390);
+            this.dgvDocenteCurso.TabIndex = 0;
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "ID";
+            this.ID.HeaderText = "ID Materias";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // IDCurso
+            // 
+            this.IDCurso.DataPropertyName = "Descripcion";
+            this.IDCurso.HeaderText = "Descripcion";
+            this.IDCurso.Name = "IDCurso";
+            // 
+            // IdDocente
+            // 
+            this.IdDocente.DataPropertyName = "HSSSemanales";
+            this.IdDocente.HeaderText = "Horas semanales";
+            this.IdDocente.Name = "IdDocente";
+            this.IdDocente.Width = 150;
+            // 
+            // Cargo
+            // 
+            this.Cargo.DataPropertyName = "HSTotales";
+            this.Cargo.HeaderText = "Horas Totales";
+            this.Cargo.Name = "Cargo";
+            this.Cargo.Width = 120;
+            // 
+            // IDPlan
+            // 
+            this.IDPlan.DataPropertyName = "IDPlan";
+            this.IDPlan.HeaderText = "ID Plan";
+            this.IDPlan.Name = "IDPlan";
             // 
             // btnSalir
             // 
@@ -118,7 +164,6 @@ namespace UI.Desktop.RegistroNotas
             this.btnSalir.TabIndex = 2;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
-            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
             // btnActualizar
             // 
@@ -129,17 +174,27 @@ namespace UI.Desktop.RegistroNotas
             this.btnActualizar.TabIndex = 1;
             this.btnActualizar.Text = "Actualizar";
             this.btnActualizar.UseVisualStyleBackColor = true;
-            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
             // 
             // tsPlanes
             // 
             this.tsPlanes.Dock = System.Windows.Forms.DockStyle.None;
             this.tsPlanes.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tsbEditar});
+            this.tsbNuevo,
+            this.tsbEditar,
+            this.tsbEliminar});
             this.tsPlanes.Location = new System.Drawing.Point(3, 0);
             this.tsPlanes.Name = "tsPlanes";
-            this.tsPlanes.Size = new System.Drawing.Size(35, 25);
+            this.tsPlanes.Size = new System.Drawing.Size(81, 25);
             this.tsPlanes.TabIndex = 0;
+            // 
+            // tsbNuevo
+            // 
+            this.tsbNuevo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbNuevo.Image = ((System.Drawing.Image)(resources.GetObject("tsbNuevo.Image")));
+            this.tsbNuevo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbNuevo.Name = "tsbNuevo";
+            this.tsbNuevo.Size = new System.Drawing.Size(23, 22);
+            this.tsbNuevo.Text = "Nuevo";
             // 
             // tsbEditar
             // 
@@ -149,24 +204,31 @@ namespace UI.Desktop.RegistroNotas
             this.tsbEditar.Name = "tsbEditar";
             this.tsbEditar.Size = new System.Drawing.Size(23, 22);
             this.tsbEditar.Text = "Editar";
-            this.tsbEditar.Click += new System.EventHandler(this.tsbEditar_Click);
             // 
-            // RegistroNotas
+            // tsbEliminar
+            // 
+            this.tsbEliminar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbEliminar.Image = ((System.Drawing.Image)(resources.GetObject("tsbEliminar.Image")));
+            this.tsbEliminar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbEliminar.Name = "tsbEliminar";
+            this.tsbEliminar.Size = new System.Drawing.Size(23, 22);
+            this.tsbEliminar.Text = "Eliminar";
+            // 
+            // RegistrarNotasMat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tcPlanes);
-            this.Name = "RegistroNotas";
-            this.Text = "RegistroNotas";
-            this.Load += new System.EventHandler(this.RegistroNotas_Load);
+            this.Name = "RegistrarNotasMat";
+            this.Text = "RegistrarNotasMat";
             this.tcPlanes.ContentPanel.ResumeLayout(false);
             this.tcPlanes.TopToolStripPanel.ResumeLayout(false);
             this.tcPlanes.TopToolStripPanel.PerformLayout();
             this.tcPlanes.ResumeLayout(false);
             this.tcPlanes.PerformLayout();
             this.tlPlanes.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvRN)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDocenteCurso)).EndInit();
             this.tsPlanes.ResumeLayout(false);
             this.tsPlanes.PerformLayout();
             this.ResumeLayout(false);
@@ -177,10 +239,17 @@ namespace UI.Desktop.RegistroNotas
 
         private System.Windows.Forms.ToolStripContainer tcPlanes;
         private System.Windows.Forms.TableLayoutPanel tlPlanes;
-        private System.Windows.Forms.DataGridView dgvRN;
+        private System.Windows.Forms.DataGridView dgvDocenteCurso;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IDCurso;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IdDocente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cargo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IDPlan;
         private System.Windows.Forms.Button btnSalir;
         private System.Windows.Forms.Button btnActualizar;
         private System.Windows.Forms.ToolStrip tsPlanes;
+        private System.Windows.Forms.ToolStripButton tsbNuevo;
         private System.Windows.Forms.ToolStripButton tsbEditar;
+        private System.Windows.Forms.ToolStripButton tsbEliminar;
     }
 }
