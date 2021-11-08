e152436eccabf3437842f4f2a48c55027fb10dbd
Luciano Ragusa <80652919+Luchoragusa@users.noreply.github.com>
2021-11-08 00:32:13 -0300

    cleanCode1

--- UI.Desktop/RegitrarDocenteCurso/Cursos-Docente.cs
+++ UI.Desktop/RegitrarDocenteCurso/Cursos-Docente.cs
@@ -1,15 +1,7 @@
 ﻿using Business.Entities;
 using Business.Logic.EntidadesLogic;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using UI.Desktop.RegistrarNotas;
 
 namespace UI.Desktop.RegitrarDocenteCurso
 {
